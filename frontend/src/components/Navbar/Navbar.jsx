a27aae2897a46499e853819978e967bb975ff1f2
HabibaJahn <priyajahn2001@gmail.com>
2025-05-10 20:29:19 +0600

    HomePage

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar(){
             <div className="left">
                   <a href="/" className="logo">
                         <img src="/logo.png" alt=""   />
-                        <span>MessFinder</span>
+                        <span>HomeHunt</span>
                   </a>
                   <a href="/">Home</a>
                   <a href="/">About</a>
---
+++ frontend/src/routes/homePage/homePage.jsx
@@ -0,0 +1,20 @@
+import './homePage.scss';
+
+function HomePage(){
+      return(
+            <div className="homePage">
+                  <div className="textContainer">
+                        <div className="wrapper">
+                        <h1 className="title">HomeHunt – Your Trusted Partner in Finding the Perfect Rental Home</h1>
+                        <p>
+                              Discover affordable, comfortable, and verified rental homes near you with ease and confidence.
+                        </p>
+                        </div>
+                  </div>
+                  <div className="imgContainer">
+                        <img src="/bg.png" alt=""/>
+                  </div>
+            </div>
+      )
+}
+export default HomePage;
