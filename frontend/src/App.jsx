baace2bff35941bd9d5058f043cc8926105cbd1f
HabibaJahn <priyajahn2001@gmail.com>
2025-05-22 20:10:29 +0600

    card

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from './routes/homePage/homePage';
 import { createBrowserRouter, RouterProvider  } from "react-router-dom";
 import ListPage from "./routes/listPage/listPage";
 import  Layout from "./routes/layout/layout";
+import SinglePage from './routes/singlePage/singlePage';
 function App() {
   
 
@@ -19,6 +20,11 @@ const router = createBrowserRouter([
           {
             path: "/list",
             element: <ListPage />,
+            
+          },
+          {
+            path: "/:id",
+            element: <SinglePage />,
             
           }
         ]
