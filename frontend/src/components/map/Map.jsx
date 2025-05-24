e22a779a244b84900ab9746447da474d7061cacc
HabibaJahn <priyajahn2001@gmail.com>
2025-05-24 20:19:42 +0600

    Singlepage

--- frontend/src/components/map/Map.jsx
+++ frontend/src/components/map/Map.jsx
@@ -1,19 +1,19 @@
 import './map.scss'
 import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
-
-function Map(){
+import "leaflet/dist/leaflet.css"
+import Pin from '../pin/Pin'
+function Map({items}){
       return(
-            <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-            <TileLayer
-                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={position}>
-                  <Popup>
-                  A pretty CSS3 popup. <br /> Easily customizable.
-                  </Popup>
-            </Marker>
+            <MapContainer center={[51.505, -0.09]} zoom={7} scrollWheelZoom={false} className='map'>
+                  <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  />
+                  {items.map(item => (
+                        <Pin item={item} key={item.id}/>
+                  ))}
             </MapContainer>
       )
-} export default Map;
+} 
+export default Map;
 
---
+++ frontend/src/components/slider/Slider.jsx
@@ -0,0 +1,8 @@
+import './slider.scss'
+
+function Slider(){
+      return(
+            <div className="Slider">Slider</div>
+      )
+}
+export default Slider;
