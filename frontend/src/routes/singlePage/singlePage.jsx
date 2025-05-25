30e83ea3a081ee8750aba6bb3233cb875069ac06
HabibaJahn <priyajahn2001@gmail.com>
2025-05-25 22:18:35 +0600

    Singlepage update

--- frontend/src/routes/singlePage/singlePage.jsx
+++ frontend/src/routes/singlePage/singlePage.jsx
@@ -1,6 +1,7 @@
 import Slider from '../../components/slider/Slider'
 import './singlePage.scss'
 import { singlePostData, userData } from '../../lib/dummydata'
+import Map from "../../components/map/Map"
 
 function SinglePage(){
       return (
@@ -25,12 +26,57 @@ function SinglePage(){
                                           </div>
                                     </div>
 
-                                    <div className="bottom"></div>
+                                    <div className="bottom">
+                                          {singlePostData.description}
+                                    </div>
                               </div>
                         </div>
                   </div>
                   <div className="Features">
-                        <div className="wrapper"></div>
+                        <div className="wrapper">
+                              <p className="title">General</p>
+                              <div className="listVertical">
+                                    <div className="feature">
+                                         <img src="/utility.png" alt ="" />
+                                         <div className="featureText">
+                                          <span>Utilities</span>
+                                          <p>Renter is responsible</p>
+                                         </div>
+                                    </div>
+                                    <div className="feature">
+                                         <img src="/pet.png" alt ="" />
+                                         <div className="featureText">
+                                          <span>Pet Policy</span>
+                                          <p>Pets Allowed</p>
+                                         </div>
+                                    </div>
+                                    <div className="feature">
+                                         <img src="/fee.png" alt ="" />
+                                         <div className="featureText">
+                                          <span>Property Fees </span>
+                                          <p>Must have 3x the rent in total household income</p>
+                                         </div>
+                                    </div>
+                              </div>
+                              <p className="title">Sizes</p>
+                              <div className="sizes"></div>
+                              <p className="title">Nearby Places</p>
+                              <div className="listHorizontal"></div>
+                              <p className="title">Location</p>
+                              <div className="mapContainer">
+                                    <Map items={[singlePostData]}/>
+                              </div>
+                              <div className="buttons">
+                                    <button>
+                                          <img src="/chat.png" alt=""/>
+                                          Send a Message
+                                    </button>
+                                    <button>
+                                          <img src="/chat.png" alt=""/>
+                                          Save the Place
+                                    </button>
+                              </div>
+                        </div>
                   </div>
             
             </div>
