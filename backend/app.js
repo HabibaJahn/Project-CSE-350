68550ea262a8fe285ddd9fc52fe7cf7ab5016026
HabibaJahn <priyajahn2001@gmail.com>
2025-06-04 23:13:34 +0600

    test done

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import postRoute  from "./routes/post.route.js";
 import authRoute  from "./routes/auth.route.js";
+import testRoute  from "./routes/test.route.js";
 import cors from "cors";
 
 const app = express();
@@ -11,6 +12,8 @@ app.use(cookieParser())
 
 app.use("/api/posts",postRoute);
 app.use("/api/auth",authRoute);
+app.use("/api/test",testRoute);
+
 app.listen(8800,() => {
       console.log("Server is running");
 });
