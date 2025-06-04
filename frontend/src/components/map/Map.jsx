0a70bd31a25390eb764a3f04aa85f8e4b47f715c
JannatunNayma16 <nayma.cse16@gmail.com>
2025-06-04 14:06:22 +0600

    nayma

--- frontend/src/components/map/Map.jsx
+++ frontend/src/components/map/Map.jsx
@@ -1,19 +1,22 @@
-import './map.scss'
-import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
-import "leaflet/dist/leaflet.css"
-import Pin from '../pin/Pin'
-function Map({items}){
-      return(
-            <MapContainer center={[51.505, -0.09]} zoom={7} scrollWheelZoom={false} className='map'>
-                  <TileLayer
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                  />
-                  {items.map(item => (
-                        <Pin item={item} key={item.id}/>
-                  ))}
-            </MapContainer>
-      )
-} 
-export default Map;
+import './Map.scss';
+import { MapContainer, TileLayer} from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';  
+import Pin from '../Pin/Pin';
 
+function Map({items}){
+    return(
+        <MapContainer center={[51.505, -0.09]} zoom={6} scrollWheelZoom={false} className='map'>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    {items.map(item=>
+      <Pin item={item} key={item.id} />
+    )}
+  </MapContainer>   
+    )
+}
+
+export default Map
+
+//rdijffkewr90
